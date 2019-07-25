e1ca88b5920a4d2bb68776537b6a1c25919031af
Jake Stevens <onegreentiger@gmail.com>
2019-07-24 20:17:36 -0500

    got the jumbotron in, need to style and update info later

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../App.css";
 import LpNav from "./navbar/LpNav";
+import LP_Jumbotron from './Jumbotron/LP_Jumbotron';
 
 var LandingPage = () => {
   return (
     <div className="LandingPage">
       <LpNav/>
-      {/* Navbar*/}
-      {/* Jumbotron */}
+      <LP_Jumbotron />
       {/* Features */}
       {/* Stories */}
       {/* Call to Action */}
