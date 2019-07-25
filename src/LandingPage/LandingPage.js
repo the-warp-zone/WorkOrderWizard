d3e3fccb46bc84e26dab7b1896dfda6780f34307
Jake Stevens <onegreentiger@gmail.com>
2019-07-24 20:53:10 -0500

    added in basic testimonials

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../App.css";
 import LpNav from "./navbar/LpNav";
 import LP_Jumbotron from './Jumbotron/LP_Jumbotron';
+import Testimonials from './Testimonials/Testimonials';
 
 var LandingPage = () => {
   return (
     <div className="LandingPage">
       <LpNav/>
       <LP_Jumbotron />
+      <Testimonials />
       {/* Features */}
       {/* Stories */}
       {/* Call to Action */}
