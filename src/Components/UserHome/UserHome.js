43e96e57e5bf0c266569087ad3447cbb9f57ff8e
JakedStevens <onegreentiger@gmail.com>
2019-07-27 11:28:39 -0500

    have search bar and nav in and completed

---
+++ src/Components/UserHome/UserHome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Searchbar from './Searchbar/Searchbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Searchbar />
+    </div>
+  );
+}
+
+export default App
