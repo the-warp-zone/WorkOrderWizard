046f477447795722659a66896869b887b5c81546
JakedStevens <onegreentiger@gmail.com>
2019-07-27 13:03:05 -0500

    styling on the cards is mostly done

--- src/Components/UserHome/PageCards/PageCards.js
+++ src/Components/UserHome/PageCards/PageCards.js
@@ -1,51 +1,62 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import './PageCards.css';
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import './PageCards.css'
 
 var PageCards = () => {
     return (
         <div>
-            <Row className='margin-top'>
-                <Col lg={{ span: 3  }}></Col>
-                <Col lg={{ span: 2  }}>
-                    <Card style={{ width: '300px', height: '200px' }} className='shadow-box'>
-                        <Card.Body>
-                            <Card.Link href="#" className='card-title'>Customers</Card.Link>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg={{ span: 1  }}></Col>
+            <Row className="margin-top">
+                <Col lg={{ span: 3 }}></Col>
                 <Col lg={{ span: 2 }}>
-                    <Card style={{ width: '300px', height: '200px' }} className='shadow-box'>
-                        <Card.Body>
-                            <Card.Link href="#" className='card-title'>Work Orders</Card.Link>
-                        </Card.Body>
-                    </Card>
+                    <a href="#" className="card-title">
+                        <Card
+                            style={{ width: '300px', height: '200px' }}
+                            className="shadow-box"
+                        >
+                            <Card.Body>Customers</Card.Body>
+                        </Card>
+                    </a>
+                </Col>
+                <Col lg={{ span: 1 }}></Col>
+                <Col lg={{ span: 2 }}>
+                    <a href="#" className="card-title">
+                        <Card
+                            style={{ width: '300px', height: '200px' }}
+                            className="shadow-box"
+                        >
+                            <Card.Body>Work Orders</Card.Body>
+                        </Card>
+                    </a>
                 </Col>
             </Row>
-            <Row className='margin-top'>
-            <Col lg={{ span: 3  }}></Col>
+            <Row className="margin-top">
+                <Col lg={{ span: 3 }}></Col>
                 <Col lg={{ span: 2 }}>
-                    <Card style={{ width: '300px', height: '200px' }} className='shadow-box'>
-                        <Card.Body>
-                            <Card.Link href="#" className='card-title'>Invoices</Card.Link>
-                        </Card.Body>
-                    </Card>
+                    <a href="#" className="card-title">
+                        <Card
+                            style={{ width: '300px', height: '200px' }}
+                            className="shadow-box"
+                        >
+                            <Card.Body>Invoices</Card.Body>
+                        </Card>
+                    </a>
                 </Col>
-                <Col lg={{ span: 1  }}></Col>
+                <Col lg={{ span: 1 }}></Col>
                 <Col lg={{ span: 2 }}>
-                    <Card style={{ width: '300px', height: '200px' }} className='shadow-box'>
-                        <Card.Body>
-                            <Card.Link href="#" className='card-title'>Costs</Card.Link>
-                        </Card.Body>
-                    </Card>
+                    <a href="#" className="card-title">
+                        <Card
+                            style={{ width: '300px', height: '200px' }}
+                            className="shadow-box"
+                        >
+                            <Card.Body>Costs</Card.Body>
+                        </Card>
+                    </a>
                 </Col>
             </Row>
-
         </div>
     )
 }
 
-export default PageCards;
+export default PageCards
