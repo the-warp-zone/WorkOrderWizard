1943a077c897c57997d6faff15b5d32c50375858
CalebRose <ctrose17@gmail.com>
2019-07-29 19:31:04 -0500

    Landing page reformatting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from "react";
-import "./App.css";
-import LandingPage from "./Components/LandingPage/LandingPage";
-import MainNav from "./Components/MainNav/MainNav";
-import UserHome from './Components/UserHome/UserHome';
+import React from 'react'
+import './App.css'
+import LandingPage from './Components/LandingPage/LandingPage'
+import MainNav from './Components/MainNav/MainNav'
+import UserHome from './Components/UserHome/UserHome'
 
 function App() {
-  return (
-    <div className="App">
-      {/* <LandingPage /> */}
+    return (
+        <div className="App">
+            <LandingPage />
 
-    {/* After user is past landing page or loggin in... */}
-      <MainNav />
-      <UserHome />
-      
-    </div>
-  );
+            {/* After user is past landing page or loggin in... */}
+            {/* <MainNav /> */}
+            {/* <UserHome /> */}
+        </div>
+    )
 }
 
 export default App
--- src/Components/LandingPage/Features/Features.js
+++ src/Components/LandingPage/Features/Features.js
@@ -1,42 +1,59 @@
-import React from "react";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import './Features.css';
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import './Features.css'
 
 var Features = () => {
     return (
         <div>
-            <hr></hr>
+            <hr />
             <Row>
-                <Col lg={{ span: 4, offset: 2 }} ><h2 ><em className='col'>Features</em></h2></Col>
-
+                {/* lg={{ span: 3, offset: 2 }} */}
+                <Col>
+                    <h2>
+                        <em className="col">Features</em>
+                    </h2>
+                </Col>
             </Row>
             <Row>
-                <Col></Col>
+                <Col />
             </Row>
             <Row>
-                <Col lg={{ span: 4, offset: 1 }}>
-                    <ul className='list'>
-                        <li >This is a feature</li>
+                {/* lg={{ span: 3, offset: 1 }} */}
+                <Col>
+                    <ul className="list">
+                        <li>This is a feature</li>
                         <li>This is also a feature</li>
                         <li>What is a feature?</li>
                         <li>This feature changed my life</li>
                         <li>This feature saved my marriage</li>
-                        <li>I broke into the natural history museum to steal this feature</li>
+                        <li>
+                            I broke into the natural history museum to steal
+                            this feature
+                        </li>
                     </ul>
                 </Col>
-                <Col lg={{ span: 3, offset: 0 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu nisi et leo imperdiet euismod. Nulla id volutpat sem. Etiam non massa vitae leo laoreet sollicitudin imperdiet nec tortor. In hac habitasse platea dictumst. Mauris sapien orci, condimentum id lorem vitae, ultricies egestas magna. Duis bibendum pulvinar velit vitae porttitor.
+                {/* lg={{ span: 3, offset: 0 }} */}
+                <Col>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer eu nisi et leo imperdiet euismod. Nulla id volutpat
+                    sem. Etiam non massa vitae leo laoreet sollicitudin
+                    imperdiet nec tortor. In hac habitasse platea dictumst.
+                    Mauris sapien orci, condimentum id lorem vitae, ultricies
+                    egestas magna. Duis bibendum pulvinar velit vitae porttitor.
                 </Col>
-                <Col lg={{ span: 3, offset: 0 }}>
-                    <img src='https://via.placeholder.com/500x200.jpg' alt='placeholder'></img>
+                {/* lg={{ span: 3, offset: 0 }} */}
+                <Col>
+                    <img
+                        src="https://via.placeholder.com/500x200.jpg"
+                        alt="placeholder"
+                    />
                 </Col>
             </Row>
 
-        //TEMPLATE
-
+            {/* //TEMPLATE */}
         </div>
-    );
-};
+    )
+}
 
-export default Features;
+export default Features
