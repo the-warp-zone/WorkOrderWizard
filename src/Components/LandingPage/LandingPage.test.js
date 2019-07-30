2d19b0b3a983fd0e526c2a3a7dbb3d22a0816b13
CalebRose <ctrose17@gmail.com>
2019-07-29 19:36:43 -0500

    fixing test

--- src/Components/LandingPage/LandingPage.test.js
+++ src/Components/LandingPage/LandingPage.test.js
@@ -11,11 +11,11 @@ describe('Landing Page', () => {
         expect(wrapper.find('div').length).toEqual(1)
     })
 
-    // it('should render the LP_Nav Component', () => {
-    //     expect(wrapper.containsMatchingElement(<LpNav />)).toEqual(true)
-    // })
+    it('should render the LP_Nav Component', () => {
+        expect(wrapper.containsMatchingElement(<LpNav />)).toEqual(true)
+    })
 
-    // it('should render the LP_Jumbotron Component', () => {
-    //     expect(wrapper.containsMatchingElement(<LP_Jumbotron />)).toEqual(true)
-    // })
+    it('should render the LP_Jumbotron Component', () => {
+        expect(wrapper.containsMatchingElement(<LP_Jumbotron />)).toEqual(true)
+    })
 })
