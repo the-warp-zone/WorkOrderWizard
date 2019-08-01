72c870d71f80149ce406dfa391aff1ab164e746d
Steve Long <steve@gridtop.com>
2019-08-01 11:20:02 -0500

    small change

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const apiRoutes = require("./api-routes.js");
 
 // Book routes
-router.use("/burger", apiRoutes);
+router.use("/apiroutes", apiRoutes);
 
 module.exports = router;
