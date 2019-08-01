68dc50460c8dbd47a90ad0924ef663f144e22d5e
Steve Long <steve@gridtop.com>
2019-08-01 17:59:26 -0500

    I forgot to add the update customer route: customer/:id with fields to update in the body in JSON

--- controllers/customer.js
+++ controllers/customer.js
@@ -60,6 +60,17 @@ router.delete("/*", function(req, res) {
 	Customer.findById(req.params[0], function (err, customer) { if(customer) customer.remove();}).then(function (customer){res.send(customer).status(200)});
 });
 
+router.put("/*", function(req, res) {
+	console.log("update customer route hit");
+	console.log(req.params); //this specifies which customer record we are adding information to
+	console.log(req.body); //use this to specify properties and values to add
+	Customer.update({_id:req.params[0]}, req.body, function(err, numAffected){
+		if (err) return console.log(err);
+		console.log("numAffected:");
+		console.log(numAffected);
+	}).then(function (customer){console.log(customer);res.send(customer).status(200)});
+});
+
 
 module.exports = router;
 
