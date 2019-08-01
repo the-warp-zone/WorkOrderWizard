385f62410ded2f50fc48263c8f44a35b6d14beb2
Steve Long <steve@gridtop.com>
2019-08-01 12:48:55 -0500

    data coming back

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
-var models = require('./models');
+const models = require('./models');
 
 const mongoose = require('mongoose');
 
