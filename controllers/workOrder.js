968fa849417d35299db19f0b188fdce5ed66b5ae
Steve Long <steve@gridtop.com>
2019-08-01 23:13:31 -0500

    still working on this feature

--- controllers/workOrder.js
+++ controllers/workOrder.js
@@ -42,4 +42,35 @@ router.put("/*", function(req, res) {
 	}).then(function (workorder){console.log(workorder);res.send(workorder).status(200)});
 });
 
+
+
+// Below are the routes for dealing with SchemaType Arrays -- arrays of invoice and cost subdocuments
+// need ability to add, remove, and update both types of subdocument (invoice and cost)
+// don't need retrieval of all, because that is covered by the get one work order.
+// may need to include a route each for the retrieval of one item.
+
+/*
+router.post("/", function(req, res) {
+	
+	let workorder = new WorkOrder(req.body).save().then(function(workorder){
+		res.send(workorder).status(200);
+	});
+});
+
+router.delete("/*", function(req, res) {
+	WorkOrder.findById(req.params[0], function (err, workorder) { if(workorder) workorder.remove();}).then(function (workorder){res.send(workorder).status(200)});
+});
+
+router.put("/*", function(req, res) {
+	console.log("update workorder route hit");
+	console.log(req.params); //this specifies which workorder record we are adding information to
+	console.log(req.body); //use this to specify properties and values to add
+	WorkOrder.update({_id:req.params[0]}, req.body, function(err, numAffected){
+		if (err) return console.log(err);
+		console.log("numAffected:");
+		console.log(numAffected);
+	}).then(function (workorder){console.log(workorder);res.send(workorder).status(200)});
+});
+*/
+
 module.exports = router;
