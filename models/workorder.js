345653b255006f1f203f512acda14ba4670b482e
Steve Long <steve@gridtop.com>
2019-08-01 22:46:58 -0500

    nesting invoices and costs inside workorders

--- models/workorder.js
+++ models/workorder.js
@@ -2,11 +2,33 @@ const mongoose = require('mongoose');
 	
 const Schema = mongoose.Schema;
 
+const invoiceSchema = new Schema({
+	paid: Boolean,
+	open: Boolean,
+	amountBilled: Number,
+	dateIssued: Date
+});
+
+const costSchema = new Schema({
+	vendorReceiptSN: String,
+	vendor: String,
+	datePurchased: Date,
+	amount: Number
+});
+
 const workOrderSchema = new Schema({
 	customerID: String,
 	createdOn: Date,
 	dateDue: Date,
-	hoursLogged: Number
+	hoursLogged: Number,
+	costs: {
+    type: [costSchema],
+    default: undefined
+	},
+	invoices: {
+    type: [invoiceSchema],
+    default: undefined
+	}
 });
 
 const WorkOrder = mongoose.model("WorkOrder", workOrderSchema);
