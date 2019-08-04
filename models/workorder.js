339a9f2be77c5f7efa7d4cd600e4e91803d1fa15
Steve Long <steve@gridtop.com>
2019-08-03 23:30:38 -0500

    added a feature where if no workorder is found by ID, to return all WOs for that customer ID

--- models/workorder.js
+++ models/workorder.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workOrderSchema = new Schema({
-	customerID: String,
 	createdOn: Date,
 	dateDue: Date,
 	hoursLogged: Number,
