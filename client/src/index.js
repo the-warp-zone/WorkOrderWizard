596df916293798fb01ce0835cc116de2ad02ff0c
Steve Long <steve@gridtop.com>
2019-08-07 20:36:08 -0500

    fixing refernce issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import * as Provider from 'react-redux';
 import { render } from 'react-dom';
-import configureStore from './store/configureStore';
+import configureStore from './.store/configureStore';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
