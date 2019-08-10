e719ec953311670c1267c2a45cd182e8d8277521
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-08-10 16:37:49 -0500

    added signout button that is a component

--- client/src/Components/MainNav/MainNav.js
+++ client/src/Components/MainNav/MainNav.js
@@ -1,64 +1,53 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import { Link } from 'react-router-dom'
+import SignOutButton from '../LandingPage/Jumbotron/SignOut'
 import Routes from '../../Constants/routes'
 import './MainNav.css'
 import { LinkContainer } from 'react-router-bootstrap'
 
-var MainNav = () => {
+const MainNav = props => (
     // Might need to render sign in and sign out separately
-    return (
-        <div>
-            <Navbar
-                collapseOnSelect
-                expand="lg"
-                className="navColors navbar-dark"
-            >
-                <LinkContainer to={Routes.HOME}>
-                    <Navbar.Brand className="navText">
-                        Work Order Wizard
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <LinkContainer to={Routes.CUSTOMERS}>
-                            <Nav.Link>Customers</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.WORK_ORDERS}>
-                            <Nav.Item>Work Orders</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.INVOICES}>
-                            <Nav.Item>Invoices</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.COSTS}>
-                            <Nav.Item>Costs</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.ARCHIVED}>
-                            <Nav.Item>Archived</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.JOBS}>
-                            <Nav.Item>Jobs</Nav.Item>
-                        </LinkContainer>
-                        */}
-                    </Nav>
-                    <Nav className="ml-auto">
-                        {/* Note: Only Show My Account While Logged In */}
-                        <LinkContainer to={Routes.ACCOUNT}>
-                            <Nav.Item>My Account</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.SIGN_IN}>
-                            <Nav.Item>Sign In</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to={Routes.LANDING}>
-                            <Nav.Item>Log Out</Nav.Item>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    )
-}
+    <div>
+        <Navbar collapseOnSelect expand="lg" className="navColors navbar-dark">
+            <Navbar.Brand className="navText" href="#">
+                Work Order Wizard
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <LinkContainer to={Routes.CUSTOMERS}>
+                        <Nav.Link>Customers</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Routes.WORK_ORDERS}>
+                        <Nav.Link>Work Orders</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Routes.INVOICES}>
+                        <Nav.Link>Invoices</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Routes.COSTS}>
+                        <Nav.Link>Costs</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Routes.ARCHIVED}>
+                        <Nav.Link>Archived</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Routes.JOBS}>
+                        <Nav.Link>Jobs</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+                <Nav className="ml-auto">
+                    <LinkContainer to={Routes.ACCOUNT}>
+                        <Nav.Link>My Account</Nav.Link>
+                    </LinkContainer>
+                    <Nav.Link onClick={props.handleShow}>Sign In</Nav.Link>
+                    <LinkContainer to={Routes.LANDING}>
+                        <Nav.Link>Log Out</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <SignOutButton />
+    </div>
+)
 
 export default MainNav
