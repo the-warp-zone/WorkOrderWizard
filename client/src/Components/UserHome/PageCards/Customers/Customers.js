ac53788bc3f5d5ba157fcb19d4b9af6d7a4b58a7
CalebRose <ctrose17@gmail.com>
2019-08-10 13:46:16 -0500

    pushing this change up

--- client/src/Components/UserHome/PageCards/Customers/Customers.js
+++ client/src/Components/UserHome/PageCards/Customers/Customers.js
@@ -1,42 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import CustomerRow from './CustomerRow'
+import axios from 'axios'
 
-var Customers = () => {
-    const url = 'http://localhost:3001/customer/all'
-    console.log('fetching:\t' + url)
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-    let CustomerData = [].map(i => {
-        return <CustomerRow />
-    })
+class Customers extends Component {
+    state = {
+        data: [],
+    }
+    componentDidMount() {
+        const url = 'http://localhost:3001/customer/all'
+        // console.log('fetching:\t' + url)
+        //var res = await axios.get(url)
 
-    return (
-        <div>
-            <h1>Customers</h1>
-            <div>
-                <Table responsive="md">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Business Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <CustomerRow />
-                    </tbody>
-                </Table>
-            </div>
-        </div>
-    )
+        fetch(url)
+            .then(response => response.json())
+            .then(data => this.setState({ data: data }))
+        //console.log(res)
+        //this.setState({ data: res.data })
+    }
+    render() {
+        return <CustomerRow data={this.state.data} />
+    }
 }
 
 export default Customers
