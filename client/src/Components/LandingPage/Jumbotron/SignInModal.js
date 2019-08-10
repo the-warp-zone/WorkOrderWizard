8be9e7a143794b099a853287c519aebcd1663c15
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-08-10 13:59:34 -0500

    it works but not pretty

--- client/src/Components/LandingPage/Jumbotron/SignInModal.js
+++ client/src/Components/LandingPage/Jumbotron/SignInModal.js
@@ -1,37 +1,104 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { withFirebase } from '../../../Constants/Firebase/'
-import * as ROUTES from '../../../Constants/routes'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import { Link, withRouter } from 'react-router-dom'
 
-const SignInModal = props => {
-    return (
-        <Modal
-            show={props.show}
-            onHide={props.close}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Sign In
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
-                </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.close}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
+import { withFirebase } from '../../../Constants/Firebase/'
+import ROUTES from '../../../Constants/routes'
+
+const SignInModal = () => (
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
+)
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
 }
 
+class SignUpFormBase extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { ...INITIAL_STATE }
+    }
+
+    onSubmit = event => {
+        const { username, email, passwordOne } = this.state
+
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                this.setState({ ...INITIAL_STATE })
+                this.props.history.push(ROUTES.ADMIN)
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
+
+        event.preventDefault()
+    }
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    render() {
+        const { username, email, passwordOne, passwordTwo, error } = this.state
+
+        const isInvalid =
+            passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === ''
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign Up
+                </button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        )
+    }
+}
+
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+)
+const SignUpForm = withRouter(withFirebase(SignUpFormBase))
+
 export default SignInModal
+
+export { SignUpForm, SignUpLink }
