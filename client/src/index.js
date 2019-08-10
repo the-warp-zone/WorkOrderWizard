85f01aecbc1ca5db36a0e6b16e20f0d61e8d015b
Steve Long <steve@gridtop.com>
2019-08-09 22:49:32 -0500

    Revert "Merge branch 'master' into rollingbackchanges"
    This reverts commit 808cfadce39ce61c92e2fde5c504b59b4ac6139e, reversing
    changes made to 520839c2df5c1b079d6cb4d5f3a686f7acdce927.

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import * as Provider from 'react-redux';
-import { render } from 'react-dom';
-import configureStore from './.store/configureStore';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore();
-
-render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
