7c5ff843caebc98ccf7940e67a7c3a238a6b37aa
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-08-10 16:28:02 -0500

    signup page shows up working

--- client/src/Components/LandingPage/Jumbotron/SignInModal.js
+++ client/src/Components/LandingPage/Jumbotron/SignInModal.js
@@ -1,43 +1,88 @@
 import React, { Component } from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import { compose } from 'recompose'
 
-import { withFirebase } from '../../../Constants/Firebase/'
-import * as ROUTES from '../../../Constants/routes'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import { SignUpLink } from './SignUpModal'
+import { withFirebase } from '../../../Constants/Firebase'
+import ROUTES from '../../../Constants/routes'
 
-const SignInModal = props => {
-    return (
-        <Modal
-            show={props.show}
-            onHide={props.close}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Sign In
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Welcome Back!</h4>
-                <p>User Log in</p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.close}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
+const SignInPage = () => (
+    <div>
+        <h1>SignIn</h1>
+        <SignInForm />
+        <SignUpLink />
+    </div>
+)
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null,
 }
 
-const SignUpLink = () => (
-    <p>
-        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </p>
-)
-const SignUpForm = withRouter(withFirebase(SignUpFormBase))
+class SignInFormBase extends Component {
+    constructor(props) {
+        super(props)
 
-export default SignInModal
+        this.state = { ...INITIAL_STATE }
+    }
 
-export { SignUpForm, SignUpLink }
+    onSubmit = event => {
+        const { email, password } = this.state
+
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE })
+                this.props.history.push(ROUTES.ADMIN)
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
+
+        event.preventDefault()
+    }
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    render() {
+        const { email, password, error } = this.state
+
+        const isInvalid = password === '' || email === ''
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign In
+                </button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        )
+    }
+}
+
+const SignInForm = compose(
+    withRouter,
+    withFirebase
+)(SignInFormBase)
+
+export default SignInPage
+
+export { SignInForm }
