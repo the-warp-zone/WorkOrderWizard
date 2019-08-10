6f36a0ab6afcc2f6598ced769943dafae7d54de4
CalebRose <ctrose17@gmail.com>
2019-08-10 13:46:16 -0500

    pushing this change up

--- client/src/Components/UserHome/PageCards/Customers/CustomerRow.js
+++ client/src/Components/UserHome/PageCards/Customers/CustomerRow.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
 
 const CustomerRow = props => {
+    console.log(props)
     // Props should be the API call to get customer data
-    return (
+    var CustomerData = props.data.map(i => (
         <tr>
             <td>
                 <Button>Create W.O.</Button>
@@ -14,12 +16,32 @@ const CustomerRow = props => {
             <td>
                 <Button>Delete Customer</Button>
             </td>
-            <td>1</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
+            <td>{i.firstName + ' ' + i.lastName}</td>
+            <td>{i.businessName}</td>
+            <td>{i.email}</td>
+            <td>{i.phone}</td>
         </tr>
+    ))
+    return (
+        <div>
+            <h1>Customers</h1>
+            <div>
+                <Table responsive="md">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Business Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>{CustomerData}</tbody>
+                </Table>
+            </div>
+        </div>
     )
 }
 export default CustomerRow
