f4e298537b16d4e9e6ef5ff54b88333f828d3732
Steve Long <steve@gridtop.com>
2019-08-10 12:12:15 -0500

    changing mongoost model

--- api/models/customer.js
+++ api/models/customer.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const customerSchema = new Schema({
-    firstName:  String,
+    businessName:  String,
+	phone: Number,
+	email: String,
+	firstName:  String,
 	lastName:  String,
 	nickName:  String,
 	address1: String,
