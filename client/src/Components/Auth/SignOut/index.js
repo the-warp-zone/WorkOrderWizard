cc26bcb0c6d4f677d6deb0ad38e313120cd9fd76
CalebRose <ctrose17@gmail.com>
2019-08-15 23:20:32 -0500

    Sign in?

--- client/src/Components/Auth/SignOut/index.js
+++ client/src/Components/Auth/SignOut/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import LinkContainer from 'react-router-bootstrap';
-import Nav from 'react-bootstrap/Nav';
-import * as firebase from 'firebase';
-import Routes from '../../../Constants/routes';
-import { withFirebase } from '../Firebase';
+import React from 'react'
+import LinkContainer from 'react-router-bootstrap'
+import Nav from 'react-bootstrap/Nav'
+import * as firebase from 'firebase'
+import Routes from '../../../Constants/routes'
+import { withFirebase } from '../Firebase'
+import Button from 'react-bootstrap/Button'
 
 const SignOutButton = ({ firebase }) => (
-  <LinkContainer to={Routes.LANDING}>
-    <Nav.Link onClick={firebase.doSignOut}>Sign Out</Nav.Link>
-  </LinkContainer>
-);
+    <button to={Routes.LANDING}>
+        <Nav.Link onClick={firebase.doSignOut}>Sign Out</Nav.Link>
+    </button>
+)
 
-export default withFirebase(SignOutButton);
+export default withFirebase(SignOutButton)
--- client/src/Components/Auth/SignUp/index.js
+++ client/src/Components/Auth/SignUp/index.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { compose } from 'recompose'
 
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
-import * as ROLES from '../../constants/roles';
+import { withFirebase } from '../Firebase'
+import ROUTES from '../../../Constants/routes'
 
 const SignUpPage = () => (
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm />
-  </div>
-);
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
+)
 
 const INITIAL_STATE = {
-  username: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
-  isAdmin: false,
-  error: null,
-};
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    isAdmin: false,
+    error: null,
+}
 
-const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use'
 
 const ERROR_MSG_ACCOUNT_EXISTS = `
   An account with this E-Mail address already exists.
@@ -30,135 +29,121 @@ const ERROR_MSG_ACCOUNT_EXISTS = `
   account is already used from one of the social logins, try
   to sign in with one of them. Afterward, associate your accounts
   on your personal account page.
-`;
+`
 
 class SignUpFormBase extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props)
 
-    this.state = { ...INITIAL_STATE };
-  }
-
-  onSubmit = event => {
-    const { username, email, passwordOne, isAdmin } = this.state;
-    const roles = {};
-
-    if (isAdmin) {
-      roles[ROLES.ADMIN] = ROLES.ADMIN;
+        this.state = { ...INITIAL_STATE }
     }
 
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
-      .then(authUser => {
-        // Create a user in your Firebase realtime database
-        return this.props.firebase.user(authUser.user.uid).set({
-          username,
-          email,
-          roles,
-        });
-      })
-      .then(() => {
-        return this.props.firebase.doSendEmailVerification();
-      })
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch(error => {
-        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
-          error.message = ERROR_MSG_ACCOUNT_EXISTS;
-        }
+    onSubmit = event => {
+        const { username, email, passwordOne } = this.state
+        const roles = {}
 
-        this.setState({ error });
-      });
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                // Create a user in your Firebase realtime database
+                return this.props.firebase.user(authUser.user.uid).set({
+                    username,
+                    email,
+                })
+            })
+            .then(() => {
+                return this.props.firebase.doSendEmailVerification()
+            })
+            .then(() => {
+                this.setState({ ...INITIAL_STATE })
+                this.props.history.push(ROUTES.HOME)
+            })
+            .catch(error => {
+                if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+                    error.message = ERROR_MSG_ACCOUNT_EXISTS
+                }
 
-    event.preventDefault();
-  };
+                this.setState({ error })
+            })
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+        event.preventDefault()
+    }
 
-  onChangeCheckbox = event => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
 
-  render() {
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      isAdmin,
-      error,
-    } = this.state;
+    onChangeCheckbox = event => {
+        this.setState({ [event.target.name]: event.target.checked })
+    }
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === '' ||
-      email === '' ||
-      username === '';
+    render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            isAdmin,
+            error,
+        } = this.state
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <label>
-          Admin:
-          <input
-            name="isAdmin"
-            type="checkbox"
-            checked={isAdmin}
-            onChange={this.onChangeCheckbox}
-          />
-        </label>
-        <button disabled={isInvalid} type="submit">
-          Sign Up
-        </button>
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === ''
 
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign Up
+                </button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        )
+    }
 }
 
 const SignUpLink = () => (
-  <p>
-    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-  </p>
-);
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+)
 
 const SignUpForm = compose(
-  withRouter,
-  withFirebase,
-)(SignUpFormBase);
+    withRouter,
+    withFirebase
+)(SignUpFormBase)
 
-export default SignUpPage;
+export default SignUpPage
 
-export { SignUpForm, SignUpLink };
+export { SignUpForm, SignUpLink }
