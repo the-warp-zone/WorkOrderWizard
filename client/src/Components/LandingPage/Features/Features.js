f010e86236173a9d2f07f27dc2c23f75e278b60c
JakedStevens <onegreentiger@gmail.com>
2019-08-15 19:54:43 -0500

    adding in background image

--- client/src/Components/LandingPage/Features/Features.js
+++ client/src/Components/LandingPage/Features/Features.js
@@ -1,59 +1,47 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
 import './Features.css'
 import '../../LandingPage/LandingPage.css';
 import '../../../App.css';
 
 var Features = () => {
     return (
-        <div>
+        <React.Fragment>
             <hr />
-            <div>
-                <div>
-                    <h2>
-                        <em className="col text-font div-title">Features</em>
-                    </h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h2>
+                            <em className="col text-font div-title">Features</em>
+                        </h2>
+                    </div>
                 </div>
+                <div className="row">
+                    <div className="col">
+                        <ul className="list text-font">
+                            <li>Manage Your Customers</li>
+                            <li>Create and Archive Work Orders</li>
+                            <li>Manage your Invoices and Billing</li>
+                            <li>Keep Track of your Costs</li>
+                        </ul>
+                    </div>
+                    <div className="col text-font">
+                        <p>Work Order Wizard is your one stop for all of your business accounting. We make sure that you get to spend your
+                            time doing what you love and helping your customers, not filling out paperwork. We want to give you the easiest 
+                            possible method of keeping track of all things business related.
+                        </p>
+                    </div>
+                </div>
+                    <div className="row">
+                        <div className="col">
+                        {/* <img
+                            style={{ height: '200px', width: '350px' }}
+                            src={deskPic}
+                            alt="desk"
+                        /> */}
+                        </div>
+                    </div>
             </div>
-            <div>
-                <div />
-            </div>
-            <div>
-
-                <div>
-                    <ul className="list text-font">
-                        <li>This is a feature</li>
-                        <li>This is also a feature</li>
-                        <li>What is a feature?</li>
-                        <li>This feature changed my life</li>
-                        <li>This feature saved my marriage</li>
-                        <li>
-                            I broke into the natural history museum to steal
-                            this feature
-                        </li>
-                    </ul>
-                </div>
-
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer eu nisi et leo imperdiet euismod. Nulla id volutpat
-                    sem. Etiam non massa vitae leo laoreet sollicitudin
-                    imperdiet nec tortor. In hac habitasse platea dictumst.
-                    Mauris sapien orci, condimentum id lorem vitae, ultricies
-                    egestas magna. Duis bibendum pulvinar velit vitae porttitor.
-                </div>
-
-                <div>
-                    <img
-                        src="https://via.placeholder.com/500x200.jpg"
-                        alt="placeholder"
-                    />
-                </div>
-            </div>
-
-
-        </div>
+        </React.Fragment>
     )
 }
 
