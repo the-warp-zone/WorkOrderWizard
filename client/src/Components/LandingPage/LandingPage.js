42c958f7af3c853497256f34aec3988bbd959bf6
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-08-16 19:16:41 -0500

    added the merged changes

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
 import LpJumbotron from './Jumbotron/LP_Jumbotron';
 import Testimonials from './Testimonials/Testimonials';
 import Features from './Features/Features';
 import UserExamples from './UserExamples/UserExamples';
 import CallToAction from './CallToAction/CallToAction';
-=======
-import React from 'react'
-import LpJumbotron from './Jumbotron/LP_Jumbotron'
-import Testimonials from './Testimonials/Testimonials'
-import Features from './Features/Features'
-import UserExamples from './UserExamples/UserExamples'
-import CallToAction from './CallToAction/CallToAction'
 import './LandingPage.css';
->>>>>>> 6dbb87ffa2359a5ff4bd2bdef52f71a09150bcf8
 
 const LandingPage = props => (
   <div className="LandingPage">
