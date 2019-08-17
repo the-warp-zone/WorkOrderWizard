c1b15be12a1ef849ac30fd8ebfe409497456e438
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-08-16 19:10:49 -0500

    IT IS DONE... I think

--- client/src/Components/Auth/Session/withAuthentication.js
+++ client/src/Components/Auth/Session/withAuthentication.js
@@ -3,7 +3,7 @@ import React from 'react';
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
-const withAuthentication = Component => {
+const withAuthentication = (Component) => {
   class WithAuthentication extends React.Component {
     constructor(props) {
       super(props);
@@ -15,7 +15,7 @@ const withAuthentication = Component => {
 
     componentDidMount() {
       this.listener = this.props.firebase.onAuthUserListener(
-        authUser => {
+        (authUser) => {
           localStorage.setItem('authUser', JSON.stringify(authUser));
           this.setState({ authUser });
         },
